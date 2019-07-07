75df4078a45451e25f14f1d653143417b16a613e
Luke Pafford <lukepafford@gmail.com>
2019-07-07 02:08:06 -0700

    Make deployment script more verbose and use full paths

--- roles/flask_app/templates/install.sh
+++ roles/flask_app/templates/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-conda env update -f environment.yml --json --quiet --verbose 2>&1 | grep 'LINKING PACKAGE'
+echo "Checking for flask_app update..."
+/var/anaconda/bin/conda env update -f {{ flask_app_installDir }}/environment.yml --json --quiet --verbose 2>&1 | grep 'LINKING PACKAGE'
 
 if [[ "$?" -eq 0 ]]; then
   echo "flask_app was updated. Now restarting flask_app"
 	systemctl restart flask_app
-fi 
+else
+  echo "No version update was detected"
+fi
